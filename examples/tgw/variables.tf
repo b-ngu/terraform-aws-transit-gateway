c6910384b647f79fe86ee26bdca1ae41fed8903c
Brian Nguyen <brian@Brians-MacBook-Pro.local>
2023-10-24 08:08:09 -0600

    RAPB-62| TGW

--- examples/tgw/variables.tf
+++ examples/tgw/variables.tf
@@ -10,18 +10,6 @@ variable "transit_gateway_name" {
     default     = null
 }
 
-# variable "amazon_side_asn" {
-#     description = "The private Autonomous System Number (ASN) for the Amazon side of a BGP session"
-#     type        = number
-#     default     = 64512
-# }
-
-# variable "auto_accept_shared_attachments" {
-#     description = "Indicates whether resource attachments are automatically accepted (valid values: `disable`, `enable`)"
-#     type        = string
-#     default     = "disable"
-# }
-
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
